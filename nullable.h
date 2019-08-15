524f17b815d50e070dcc2c4a7610c72ed4e79579
collectionman <julianllorensi@outlook.com>
2019-08-14 23:38:25 -0300

    Methods signature modified

--- nullable.h
+++ nullable.h
@@ -2,7 +2,7 @@ template <typename ObjectType>
 class Nullable {
 public:
     Nullable(void) : _value(), _isSet(false) {}
-    Nullable(ObjectType value) : _value(value), _isSet(false) {}
+    Nullable(const ObjectType& value) : _value(value), _isSet(false) {}
     Nullable(const Nullable& nullable) : _value(nullable._value), _isSet(nullable._isSet) {}
     ~Nullable(void) { reset() ; }
 
@@ -12,21 +12,21 @@ public:
         swap(a._value, b._value) ;
     }
 
-    Nullable& operator = (Nullable& nullable) {
+    Nullable& operator = (const Nullable& nullable) {
         set(nullable._value) ;
         return *this ;
     }
 
-    ObjectType operator = (ObjectType value) {
+    ObjectType operator = (const ObjectType& value) {
         set(value) ;
         return _value ;
     }
 
-    bool operator == (const Nullable& nullable) {
+    bool operator == (const Nullable& nullable) const {
         return ((nullable._isSet && _isSet) && (nullable._value == _value)) ;
     }
 
-    bool operator == (ObjectType value) {
+    bool operator == (const ObjectType& value) const {
         return (_isSet && value == _value) ;
     }
 
@@ -42,7 +42,7 @@ private:
     ObjectType _value ;
     bool _isSet ;
 
-    void set(ObjectType value) {
+    void set(const ObjectType& value) {
         _value = value ;
         _isSet = true ;
     }
